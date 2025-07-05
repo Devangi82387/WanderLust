9a796fcded25cf7a0a55cfa5de12d4cf78a205ff
Devangi82387 <heydevangi82@gmail.com>
2025-07-06 00:23:09 +0530

    add images functionality

--- routes/listing.js
+++ routes/listing.js
@@ -8,9 +8,15 @@ const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
 
+const multer = require("multer");
+const upload = multer({dest : "uploads/"});
+
 router.route("/")
 .get(wrapAsync(listingController.index))
-.post(validateListing, isLoggedIn, wrapAsync (listingController.createListing));
+// .post(validateListing, isLoggedIn, wrapAsync (listingController.createListing));
+.post(upload.single("listing[image]"), (req, res)=>{
+    res.send(req.file);
+});
 
 //new route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
