42a05dfc9fb30d8d61f218df1aee067e1b688319
Devangi82387 <heydevangi82@gmail.com>
2025-07-06 12:09:19 +0530

    cloudinary image work

--- routes/listing.js
+++ routes/listing.js
@@ -9,14 +9,14 @@ const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 
 const multer = require("multer");
-const upload = multer({dest : "uploads/"});
+
+const {storage} = require("../cloudConfig");
+
+const upload = multer({storage});
 
 router.route("/")
 .get(wrapAsync(listingController.index))
-// .post(validateListing, isLoggedIn, wrapAsync (listingController.createListing));
-.post(upload.single("listing[image]"), (req, res)=>{
-    res.send(req.file);
-});
+.post(validateListing, upload.single("listing[image][url]"), isLoggedIn, wrapAsync (listingController.createListing));
 
 //new route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
